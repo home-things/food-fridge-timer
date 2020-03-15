b77f228276d80720b9ffd63cbce6b8e884f734ce
Svetlana Emelyanova <svetlana@MacBook-Air-Svetlana.local>
2020-03-15 18:02:53 +0300

    Света накодила, я тупой

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
 
-#define   CONTRAST_PIN   A2
-#define   BACKLIGHT_PIN  A3
-#define   CONTRAST       110
-//                rs  en  d0 d1 d2 d3 backlight
+//                rs  en  d0 d1 d2 d3
 LiquidCrystal lcd(12, 13, 4, 5, 6, 7);
+#define PERIOD_PIN A1
+#define VIBRO_PIN 9
+unsigned prevPeriod = 0;
+unsigned minPeriod = 2, periodSteps = 4; // 1, 2, 3, 4, 5
 
 
 // Creat a set of new characters
@@ -45,37 +46,52 @@ byte frownie[8] = {
 void setup()
 {
   Serial.begin ( 57600 );
-
-  // Switch on the backlight and LCD contrast levels
-  pinMode(BACKLIGHT_PIN, OUTPUT);
-  pinMode(CONTRAST_PIN, OUTPUT);
-
-  analogWrite(BACKLIGHT_PIN, (3.3 / 5) * 255);
-  analogWrite(CONTRAST_PIN, CONTRAST);
-
-  //lcd.setBacklightPin ( BACKLIGHT_PIN, POSITIVE );
-  //lcd.setBacklight ( HIGH );
-  lcd.backlight();
+  pinMode(PERIOD_PIN, INPUT);
+  pinMode(VIBRO_PIN, OUTPUT);
 
   lcd.begin(16,2);               // initialize the lcd
 
-  lcd.createChar (0, smiley);    // load character to the LCD
-  lcd.createChar (1, armsUp);    // load character to the LCD
-  lcd.createChar (2, frownie);   // load character to the LCD
+  // lcd.createChar (0, smiley);    // load character to the LCD
+  // lcd.createChar (1, armsUp);    // load character to the LCD
+  // lcd.createChar (2, frownie);   // load character to the LCD
 
-  lcd.home ();                   // go home
-  lcd.print("Hello, ARDUINO ");
+  lcd.home();                   // go home
+  lcd.print("FridgeFood timer");
   lcd.setCursor ( 0, 1 );        // go to the next line
-  lcd.print (" FORUM - fm   ");
+  // lcd.print ("  press  start  ");
+  lcd.setCursor ( 0, 1 );        // go to the next line
+  // lcd.cursor();
+  // lcd.blink();
+
 }
 
 void loop()
 {
-  // Do a little animation by writing to the same location
-  lcd.setCursor ( 14, 1 );
-  lcd.print (char(2));
-  delay (200);
-  lcd.setCursor ( 14, 1 );
-  lcd.print ( char(0));
-  delay (200);
+  unsigned raw = 0;
+  raw = analogRead(PERIOD_PIN); // 0 .. 1023 
+  
+  unsigned period = minPeriod + periodSteps * (1 - ((1 + raw) / 1024.0));
+  
+  if (period != prevPeriod) {
+    digitalWrite(VIBRO_PIN, HIGH);
+    delay(100);
+    digitalWrite(VIBRO_PIN, LOW);
+  }
+
+  lcd.setCursor(0, 1);
+  lcd.print("     ");
+  lcd.setCursor(0, 1);
+  lcd.print(period); // 1 .. 5
+
+  prevPeriod = period;
+
+  delay(200);
+
+  // // Do a little animation by writing to the same location
+  // lcd.setCursor ( 14, 1 );
+  // lcd.print (char(2));
+  // delay (200);
+  // lcd.setCursor ( 14, 1 );
+  // lcd.print ( char(0));
+  // delay (200);
 }
