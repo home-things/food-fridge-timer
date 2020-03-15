90d6505ce242062ea6aeb6922d1b49ff59938cc0
Svetlana Emelyanova <svetlana@MacBook-Air-Svetlana.local>
2020-03-16 01:02:28 +0300

    button debounce. pending animation

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
-#include <string.h>
+#include <EEPROM.h>
 
 //                rs  en  d0 d1 d2 d3
 LiquidCrystal lcd(12, 13, 4, 5, 6, 7);
@@ -12,12 +12,19 @@ LiquidCrystal lcd(12, 13, 4, 5, 6, 7);
 #define BUTTON_PIN 2
 
 //
+// eeprom memory addresses
+#define PERIOD_MEM 0
+#define STARTED_AT 1
+
+//
 // consts
 #define MAX_PERIOD 5
 
-unsigned prevPeriod = 0;
-unsigned minPeriod = 2, periodSteps = 4; // 1, 2, 3, 4, 5
-unsigned period; // scaleLen
+byte prevPeriod = 0;
+byte minPeriod = 2, periodSteps = 4; // 1, 2, 3, 4, 5
+byte period; // scaleLen
+byte iterationIndex = false;
+unsigned long lastBtnPressAt = 0;
 
 enum STATE {
   WELCOME,
@@ -85,8 +92,7 @@ byte frownie[8] = {
   0b10001
 };
 
-byte topRoundArrow[8] = 
-{
+byte topRoundArrow[8] = {
 	0b11111,
 	0b00001,
 	0b01101,
@@ -97,8 +103,7 @@ byte topRoundArrow[8] =
 	0b10000,
 };
 
-byte bottomRoundArrow[8] = 
-{
+byte bottomRoundArrow[8] = {
 	0b00001,
 	0b00001,
 	0b00001,
@@ -109,27 +114,37 @@ byte bottomRoundArrow[8] =
 	0b11111,
 };
 
-void printScale (unsigned day, unsigned period) {
+void printScale (byte day, byte period) {
   lcd.setCursor(0, 1);
-  for (unsigned i = 0; i < day; ++i) {
+  for (byte i = 0; i < day; ++i) {
     lcd.print("\4\4\3");
   }
-  for (unsigned i = day; i < period; ++i) {
+  for (byte i = day; i < period; ++i) {
     lcd.print("\3\3\3");
   }
-  for (unsigned i = period; i < MAX_PERIOD; ++i) {
+  for (byte i = period; i < MAX_PERIOD; ++i) {
     lcd.print("   ");
   }
   lcd.setCursor(14, 1);
   lcd.print(" "); // clear trailing \3
 }
 
+void vibroBeep () {
+  digitalWrite(VIBRO_PIN, HIGH);
+  for(size_t i = 0; i < 2e4; ++i);
+  digitalWrite(VIBRO_PIN, LOW);
+}
+
 void handlePress () {
+  if (millis() - lastBtnPressAt < 300) return;
+  lastBtnPressAt = millis();
+
   if (state == WASTED || state == PENDING) {
     state = INITIAL;
   } else {
     state = (STATE)(state + 1);
   }
+  vibroBeep();
 }
 
 void setup()
@@ -138,7 +153,7 @@ void setup()
 
   pinMode(PERIOD_PIN, INPUT);
   pinMode(VIBRO_PIN, OUTPUT);
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_PIN, INPUT);
 
   lcd.begin(16,2);
   lcd.home();
@@ -148,7 +163,9 @@ void setup()
   lcd.createChar (5, topRoundArrow);
   lcd.createChar (6, bottomRoundArrow);
 
-  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), handlePress, FALLING);
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), handlePress, RISING);
+  
+  state = INITIAL;
 }
 
 void loop()
@@ -169,9 +186,8 @@ void loop()
       period = minPeriod + periodSteps * (1 - ((1 + raw) / 1024.0));
       
       if (period != prevPeriod) {
-        digitalWrite(VIBRO_PIN, HIGH);
-        delay(133);
-        digitalWrite(VIBRO_PIN, LOW);
+        EEPROM.write(PERIOD_MEM, period);
+        vibroBeep();
       }
       printScale(0, period);
       lcd.print(period);
@@ -180,9 +196,16 @@ void loop()
       break;
     }
     case PENDING: {
-      unsigned day = 0;
+      byte day = 0;
       printScale(day, period);
       lcd.print(day);
+      // EEPROM.write(STARTED_AT, time());
+      lcd.setCursor(0, 0);
+      lcd.print(iterationIndex >= 5 ? "*" : " ");
+      lcd.setCursor(15, 0);
+      lcd.print(iterationIndex >= 5 ? "*" : " ");
+
+      iterationIndex = iterationIndex == 9 ? 0 : 1 + iterationIndex;
 
       break;
     }
